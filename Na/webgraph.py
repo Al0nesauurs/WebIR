a202fd8718d2872dc59971f35a5152d37433c777
KID <nakid001@gmail.com>
2017-11-09 18:22:11 +0700

    chklink append [0] ok

--- Na/webgraph.py
+++ Na/webgraph.py
@@ -11,24 +11,23 @@ for i in range (len(urlline)):
     print(urlline[i])
 urlmap.close()
 
-for num in range (len(urlline)-1):  
-  f = open("./spiders/html/"+urlline[num].rstrip())
-  temp = urlline[num].split('/')
-  temp[len(temp)-1] = ''
-  UrlPath = ""
-  for i in range (len(temp)-1) :
-      UrlPath += temp[i] + '/'
-  soup = BeautifulSoup(f, 'html.parser')
-  i = 0
-  chklink = []
-  for link in soup.find_all('a'):
-      if link.get('href')[len(link.get('href'))-1] == 'l':
-        print(link.get('href'))
-        chklink.append(link.get('href'))
-      
-  
-  print (chklink[0])
-  if chklink[0] in urlline or UrlPath+chklink[0] in urlline:
-      print ("YAY")
-  else:
-      print ("NAY")
+f = open("./spiders/html/"+urlline[0].rstrip())
+temp = urlline[0].split('/')
+temp[len(temp)-1] = ''
+UrlPath = ""
+for i in range (len(temp)-1) :
+    UrlPath += temp[i] + '/'
+soup = BeautifulSoup(f, 'html.parser')
+i = 0
+chklink = []
+for link in soup.find_all('a'):
+    if link.get('href')[len(link.get('href'))-1] == 'l':
+      print(link.get('href'))
+      chklink.append(link.get('href'))
+    
+
+print (chklink[0])
+if chklink[0] in urlline or UrlPath+chklink[0] in urlline:
+    print ("YAY")
+else:
+    print ("NAY")
